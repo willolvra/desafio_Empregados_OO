51039f6f04fa9f4675334c61127983cc09b1fcaa
willolvra <williamolaz@outlook.com>
2025-03-12 16:03:08 -0300

    Começo da classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,46 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Address;
+import entities.Department;
+import entities.Employee;
+
 public class Program {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nome do departamento: ");
+		String departmentName = sc.nextLine();
+		System.out.print("Dia do pagamento: ");
+		int day = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Telefone: ");
+		String phoneNumber = sc.nextLine();
+		Address address = new Address(email, phoneNumber);
+		Department department = new Department(departmentName, day, address);
+		
+		System.out.print("Quantos funcionários tem o departamento? ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			sc.nextLine();
+			System.out.printf("Dados do funcionário %d:\n", i);
+			System.out.print("Nome: ");
+			String name = sc.nextLine();
+			System.out.print("Salário: ");
+			double salary = sc.nextDouble();
+			Employee employees = new Employee(name, salary);
+			department.addEmployee(employees);
+		}
+		
+		
+		sc.close();
+	}
 
 }
